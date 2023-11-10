09ffd4cb86447d9de94fdeaf783f7245f93cca87
Xuthyst <128191705+Xuthyst@users.noreply.github.com>
2023-11-10 14:20:56 -0600

    test

--- src/SendDataScript/SendDataScript.py
+++ src/SendDataScript/SendDataScript.py
@@ -7,15 +7,15 @@ def on_publish(client,userdata,result):             #create function for callbac
     print("data published to thingsboard \n")
     pass
 #Serial connection
-ser = serial.Serial(
-    port='COM3',\
-    baudrate=115200,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-    timeout=200\
-)
-print("Connected to: " + ser.portstr + "\n")
+#ser = serial.Serial(
+#    port='COM3',\
+#    baudrate=115200,\
+#    parity=serial.PARITY_NONE,\
+#    stopbits=serial.STOPBITS_ONE,\
+#    bytesize=serial.EIGHTBITS,\
+#    timeout=200\
+#)
+#print("Connected to: " + ser.portstr + "\n")
 #Datos para la conexión con el cliente:
 # broker = "iot.eie.ucr.ac.cr"
 # port = 1883
@@ -30,28 +30,32 @@ print("Connected to: " + ser.portstr + "\n")
 i = 0
 j = 0
 lista = []
+data_captured = [['luces', 0.0], ['musica', 0.0], ['puerta', 0.99609], ['ruido', 0.0]]
 dictionary = dict()
-while i<16:#Taking 3 test 
-    data_captured = ser.readline().decode().strip().split()
-    data_captured[1] = float(data_captured[1])
+#while i<16:#Taking 3 test 
+while i<4:
+    #data_captured = ser.readline().decode().strip().split()
+    #data_captured[1] = float(data_captured[1])
     lista.append(data_captured)
-    i+=1   
-    j+=1
+    #i+=1   
+    #j+=1
     val_max = 0
     command_selected = ""
-    if(j == 4):
-        for item in lista:
-            if(item[1] > val_max):
-                val_max = item[1]
-                command_selected = item[0]
-        dictionary["command"] = command_selected
-        output = json.dumps(dictionary)
-        #print(output)
-        #client.publish(topic, output)
-        print("Los datos recibidos son:")
-        print(lista)
-        print("Identificando comando recibido...")
-        print("El comando es: " + command_selected + "\n")
-        j = 0
-        lista = []
-ser.close()
+    #if(j == 4):
+    for item in lista[0]:
+        print(item)
+        if(item[1] > val_max):
+            val_max = item[1]
+            command_selected = item[0]
+    dictionary["command"] = command_selected
+    output = json.dumps(dictionary)
+    #print(output)
+    #client.publish(topic, output)
+    print("Los datos recibidos son:")
+    #print(lista)
+    print("Identificando comando recibido...")
+    print("El comando es: " + command_selected + "\n")
+    #j = 0
+    lista = []
+    i+=1
+#ser.close()
