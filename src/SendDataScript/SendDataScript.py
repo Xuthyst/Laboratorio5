8db37cf644e04b36aad0d69d831f6f47482c75f4
Sebastian <64702743+cebherrera@users.noreply.github.com>
2023-11-15 13:48:18 -0600

    Sebddata

--- src/SendDataScript/SendDataScript.py
+++ src/SendDataScript/SendDataScript.py
@@ -1,61 +1,62 @@
 #!/usr/bin/python3
-import serial
 import paho.mqtt.client as mqtt
 import json
 #Functions
 def on_publish(client,userdata,result):             #create function for callback
     print("data published to thingsboard \n")
     pass
-#Serial connection
-#ser = serial.Serial(
-#    port='COM3',\
-#    baudrate=115200,\
-#    parity=serial.PARITY_NONE,\
-#    stopbits=serial.STOPBITS_ONE,\
-#    bytesize=serial.EIGHTBITS,\
-#    timeout=200\
-#)
-#print("Connected to: " + ser.portstr + "\n")
+
 #Datos para la conexión con el cliente:
-# broker = "iot.eie.ucr.ac.cr"
-# port = 1883
-# topic = "v1/devices/me/telemetry"
-# username = "STM"
-# password = "ozwnzj5hfdmn1nyjlxev"    
-# #Establecemos los parámetros de conexión
-# client = mqtt.Client(username)
-# client.on_publish = on_publish
-# client.username_pw_set(password)
-# client.connect(broker, port, keepalive=60)
+broker = "iot.eie.ucr.ac.cr"
+port = 1883
+topic = "v1/devices/me/telemetry"
+topicreq = "v1/devices/me/attributes/request/1"
+username = "NANO_10_SD"
+password = "r83olk7lufc6wh3yj7wo"    
+#Establecemos los parámetros de conexión
+client = mqtt.Client(username)
+client.on_publish = on_publish
+client.username_pw_set(password)
+client.connect(broker, port, keepalive=60)
+
 i = 0
-j = 0
+x = 0
 lista = []
 data_captured = [['luces', 0.0], ['musica', 0.0], ['puerta', 0.99609], ['ruido', 0.0]]
-dictionary = dict()
-#while i<16:#Taking 3 test 
-while i<4:
-    #data_captured = ser.readline().decode().strip().split()
-    #data_captured[1] = float(data_captured[1])
-    lista.append(data_captured)
-    #i+=1   
-    #j+=1
+dictionary = dict() 
+
+while (1):  
+    i+=1
     val_max = 0
     command_selected = ""
-    #if(j == 4):
-    for item in lista[0]:
-        print(item)
+    for item in data_captured:
+        #print(item)
         if(item[1] > val_max):
             val_max = item[1]
             command_selected = item[0]
+    
     dictionary["command"] = command_selected
     output = json.dumps(dictionary)
-    #print(output)
+    
+    if i>5:
+        print(dictionary)
+        print(output)
+
+    if i>2000:
+        for ind in range(len(data_captured)):
+            if x == ind:    
+                data_captured[ind][1] = 0.99609
+            else:
+                data_captured[ind][1] = 0.0 
+        i=0
+        x+=1
+        if x == 4:
+            x = 0
+    
     #client.publish(topic, output)
-    print("Los datos recibidos son:")
-    #print(lista)
-    print("Identificando comando recibido...")
-    print("El comando es: " + command_selected + "\n")
-    #j = 0
-    lista = []
-    i+=1
-#ser.close()
+    client.publish(topicreq, output)
+
+    #print("Los datos recibidos son:")
+    #print(data_captured)
+    #print("Identificando comando recibido...")
+    #print("El comando es: " + command_selected + "\n")
